893b89595f6030ff0af292903b97a5c2dd060bed
David Winfield <david.winfield2000@gmail.com>
2026-03-09 05:03:02 -0400

    configured basic lsp, installed blink.cmp, mason-lspconfig, and mason

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,13 @@
+-- line number
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- tabs
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- misc
 vim.opt.clipboard = "unnamedplus"
 vim.opt.cursorline = true
+
