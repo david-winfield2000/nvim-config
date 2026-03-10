2acb8c6c0b042cccece7e9bd6f99a9a06ba65d7c
David Winfield <david.winfield@disney.com>
2026-03-10 10:08:00 -0400

    added auto format on lint

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,8 @@
 local builtin = require("telescope.builtin")
 
 -- telescope keymaps
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {desc="Find Files"})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {desc="Grep in Files"})
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep in Files" })
 
 -- neo tree keymaps
-vim.keymap.set("n","<leader>e",":Neotree filesystem toggle left<CR>")
-
+vim.keymap.set("n", "<leader>e", ":Neotree filesystem toggle left<CR>")
