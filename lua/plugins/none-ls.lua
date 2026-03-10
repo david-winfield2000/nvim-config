2ce979871aaf477ea984b26827566ccb9d012f15
David Winfield <david.winfield@disney.com>
2026-03-10 12:23:33 -0400

    added formatting and linting with ruff and black

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,8 +8,10 @@ return {
 
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.completion.spell,
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
 			},
 			on_attach = function(client, bufnr)
 				if client.supports_method("textDocument/formatting") then
