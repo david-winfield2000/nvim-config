44bb0b901f378d003856b5a01258f87a5ab17ffa
David Winfield <david.winfield2000@gmail.com>
2026-03-10 01:58:07 -0400

    removed breaking eslint line in none-ls config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,7 +9,6 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.completion.spell,
-				require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
 			},
 		})
 	end,
