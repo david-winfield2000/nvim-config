92e14545788a3f465ba833d6df79627eb02a5052
David Winfield <david.winfield2000@gmail.com>
2026-03-10 22:15:48 -0400

    removed lspconfig since it's handled in mason config file. removed hardcoded ruff section of init.lua

--- init.lua
+++ init.lua
@@ -2,17 +2,3 @@
 require("config.lazy")
 require("config.keymaps")
 require("config.options")
-
--- ruff settings
-vim.lsp.config("ruff", {
-	init_options = {
-		settings = {
-			args = {
-				"--line-length=88", -- max line length
-				"--select=E,F,W", -- enable error, formatting, warning codes
-			},
-		},
-	},
-})
-
-vim.lsp.enable("ruff")
