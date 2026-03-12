43f741edcb4f78f237e210ec507349ea4920860d
David Winfield <david.winfield@disney.com>
2026-03-12 11:16:11 -0400

    updated config options to display errors

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,3 +11,10 @@ vim.opt.expandtab = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.cursorline = true
 
+-- inline diagnostics
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	underline = true,
+	update_in_insert = true,
+})
