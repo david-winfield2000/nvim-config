807caba5f842807e5f59a609f39091b60c9077bf
David Winfield <david.winfield2000@gmail.com>
2026-03-15 03:27:07 -0400

    added additional telescope keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,8 +3,13 @@ vim.keymap.set("n", "<leader>l", ":Lazy<CR>", { noremap = true, silent = true })
 
 -- telescope keymaps
 local telescope = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "Find Files" })
-vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "Grep in Files" })
+vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "Find files" })
+vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "Live grep" })
+vim.keymap.set("n", "<leader>fb", telescope.buffers, { desc = "Buffers" })
+vim.keymap.set("n", "<leader>fh", telescope.help_tags, { desc = "Help tags" })
+vim.keymap.set("n", "<leader>fc", function()
+	telescope.find_files({ cwd = vim.fn.stdpath("config") })
+end, { desc = "Find config files" })
 
 -- mason keymaps
 vim.keymap.set("n", "<leader>m", ":Mason<CR>", { noremap = true, silent = true })
