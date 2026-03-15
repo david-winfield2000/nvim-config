8b0777ac96c5df9380f4ca7bab08f2449c05ae8d
David Winfield <david.winfield2000@gmail.com>
2026-03-15 03:32:42 -0400

    added VERY NECESSARY keymaps to enhance telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,20 @@
 return {
-    'nvim-telescope/telescope.nvim', version = '*',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- optional but recommended
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    }
+	"nvim-telescope/telescope.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		-- optional but recommended
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
+	config = function()
+		local actions = require("telescope.actions")
+		require("telescope").setup({
+			defaults = {
+				mappings = {
+					i = { ["<C-d>"] = actions.delete_buffer }, -- insert mode
+					n = { ["<C-d>"] = actions.delete_buffer }, -- normal mode
+				},
+			},
+		})
+	end,
 }
