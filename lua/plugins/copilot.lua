81e8a85acbd7a1ae77529cb0f83c5752da6a103c
David Winfield <david.winfield2000@gmail.com>
2026-04-03 03:16:10 -0400

    commented out copilot extension

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,14 +1,14 @@
-return {
-	"github/copilot.vim",
-	lazy = false,
-	config = function()
-		-- Disable the default <Tab> mapping
-		vim.g.copilot_no_tab_map = true
-
-		-- Map Ctrl+J to accept Copilot suggestions
-		vim.keymap.set("i", "<C-L>", 'copilot#Accept("\\<CR>")', {
-			expr = true,
-			replace_keycodes = false,
-		})
-	end,
-}
+-- return {
+-- 	"github/copilot.vim",
+-- 	lazy = false,
+-- 	config = function()
+-- 		-- Disable the default <Tab> mapping
+-- 		vim.g.copilot_no_tab_map = true
+--
+-- 		-- Map Ctrl+J to accept Copilot suggestions
+-- 		vim.keymap.set("i", "<C-L>", 'copilot#Accept("\\<CR>")', {
+-- 			expr = true,
+-- 			replace_keycodes = false,
+-- 		})
+-- 	end,
+-- }
