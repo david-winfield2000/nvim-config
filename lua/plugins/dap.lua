1f00227ca33111602371416ad2d5fc0ac6e22887
David Winfield <david.winfield2000@gmail.com>
2026-04-02 23:18:54 -0400

    added dap and dap-ui plugins

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,44 @@
+return {
+	"mfussenegger/nvim-dap",
+	config = function()
+		vim.api.nvim_set_hl(0, "DapBreakpointSign", { fg = "#f14c4c" })
+		vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DapBreakpointSign" })
+	end,
+	keys = {
+		{
+			"<leader>db",
+			function()
+				require("dap").toggle_breakpoint()
+			end,
+			desc = "Toggle Breakpoint",
+		},
+		{
+			"<leader>F5",
+			function()
+				require("dap").continue()
+			end,
+			desc = "Continue",
+		},
+		{
+			"<leader>F10",
+			function()
+				require("dap").step_into()
+			end,
+			desc = "Step Into",
+		},
+		{
+			"<leader>F11",
+			function()
+				require("dap").step_over()
+			end,
+			desc = "Step Over",
+		},
+		{
+			"<leader>F12",
+			function()
+				require("dap").step_out()
+			end,
+			desc = "Step Out",
+		},
+	},
+}
