a50f5dd680c6fba7f10c7fa5da378f5c826fb830
David Winfield <david.winfield2000@gmail.com>
2026-04-04 10:12:41 -0400

    added nvim-cmp plugin

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,64 @@
+return {
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-vsnip",
+			"hrsh7th/vim-vsnip",
+		},
+		config = function()
+			local cmp = require("cmp")
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						vim.fn["vsnip#anonymous"](args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<Tab>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "vsnip" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+
+			-- cmdline
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = { { name = "buffer" } },
+			})
+
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
+			})
+
+			-- LSP
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			-- Default
+			vim.lsp.config("lua_ls", {
+				capabilities = capabilities,
+			})
+
+			-- Additional lsp support
+			vim.lsp.config("pyright", {
+				capabilities = capabilities,
+			})
+
+			vim.lsp.enable({ "lua_ls", "pyright" })
+		end,
+	},
+}
