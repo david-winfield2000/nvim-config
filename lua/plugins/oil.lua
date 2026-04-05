b30fbd1d3d4fa86fae0d92d3282afc510d986ce8
David Winfield <david.winfield2000@gmail.com>
2026-04-04 22:24:17 -0400

    added ability to toggle oil menu

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -6,6 +6,24 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	lazy = false,
 	keys = {
-		{ "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+		{
+			"<leader>e",
+			function()
+				local oil = require("oil")
+
+				-- if already in oil, close it
+				if vim.bo.filetype == "oil" then
+					vim.cmd("bd")
+					return
+				end
+
+				-- open left vertical split
+				vim.cmd("topleft vsplit")
+				vim.cmd("vertical resize 30")
+
+				oil.open()
+			end,
+			desc = "Toggle Oil",
+		},
 	},
 }
