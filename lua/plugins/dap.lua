d6a0bee1ecac77f1d64e3afc6aeeaccd16b1be89
David Winfield <david.winfield2000@gmail.com>
2026-04-05 14:11:13 -0400

    added generic setup for dap with projects

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,15 +1,23 @@
 return {
 	"mfussenegger/nvim-dap",
 	config = function()
+		local dap = require("dap")
+
 		vim.api.nvim_set_hl(0, "DapBreakpointSign", { fg = "#f14c4c" })
 		vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DapBreakpointSign" })
 
-		-- use dap for python
+		-- python dap setup
 		dap.adapters.python = {
 			type = "executable",
 			command = "python",
 			args = { "-m", "debugpy.adapter" },
 		}
+
+		-- look for any project lua dap config
+		local project_configs = vim.fn.globpath(vim.fn.getcwd(), ".nvim/*.lua", false, true)
+		for _, file in ipairs(project_configs) do
+			dofile(file)
+		end
 	end,
 	keys = {
 		{
