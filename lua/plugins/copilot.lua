8fce80db0b45e99beb8e1464a345b6606398d346
David Winfield <david.winfield2000@gmail.com>
2026-04-05 09:53:48 -0400

    added back copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+	"github/copilot.vim",
+	lazy = false,
+	config = function()
+		-- Disable the default <Tab> mapping
+		vim.g.copilot_no_tab_map = true
+
+		-- Map Ctrl+L to accept Copilot suggestions
+		vim.keymap.set("i", "<C-L>", 'copilot#Accept("\\<CR>")', {
+			expr = true,
+			replace_keycodes = false,
+		})
+	end,
+}
