516fded433945a62f90a9089711dc3d6858837e2
David Winfield <david.winfield@disney.com>
2026-04-14 13:38:28 -0400

    added additional options to oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,10 +2,20 @@ return {
 	"stevearc/oil.nvim",
 	---@module 'oil'
 	---@type oil.SetupOpts
-	opts = {},
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	lazy = false,
 	keys = {
 		{ "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
 	},
+	opts = {
+		skip_confirm_for_simple_edits = true,
+		lsp_file_methods = {
+			enabled = true,
+			timeout_ms = 1000,
+			autosave_changes = true,
+		},
+	},
+	config = function(_, opts)
+		require("oil").setup(opts)
+	end,
 }
