96cb20676bf59e71301dd6ec462e6f2564a3c3c1
David Winfield <david.winfield@disney.com>
2026-04-14 13:38:18 -0400

    added keymap for lsp rename

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,5 +3,6 @@ vim.keymap.set("n", "<leader>l", ":Lazy<CR>", { noremap = true, silent = true })
 
 -- misc keymaps
 vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP rename" })
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
