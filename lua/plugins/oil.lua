2d9d0a64a615ad99e8f69f463d69993ac3edf826
David Winfield <david.winfield2000@gmail.com>
2026-04-17 00:53:07 -0400

    reverted oil change to skip confirm

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -8,7 +8,6 @@ return {
 		{ "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
 	},
 	opts = {
-		skip_confirm_for_simple_edits = true,
 		lsp_file_methods = {
 			enabled = true,
 			timeout_ms = 1000,
