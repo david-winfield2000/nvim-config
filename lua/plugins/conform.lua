0dd8dda6918de33459ba7a004de2261d1581e572
David Winfield <david.winfield@disney.com>
2026-04-20 12:22:27 -0400

    set up conform, ripped out none-ls

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "ruff", "isort" },
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	},
+	config = function(_, opts)
+		require("conform").setup(opts)
+	end,
+}
