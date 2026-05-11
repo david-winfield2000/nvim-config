925107f305fabcf50097913d8e8481d9ace5eea0
David Winfield <david.winfield@disney.com>
2026-05-11 08:17:37 -0400

    removed redundant keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,5 +2,4 @@
 vim.keymap.set("n", "<leader>l", ":Lazy<CR>", { noremap = true, silent = true })
 
 -- misc keymaps
-vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP rename" })
