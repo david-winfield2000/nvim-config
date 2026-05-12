8942526ca5f4ca6ad9e8e6212c5fd25cb90b06d8
David Winfield <david.winfield@disney.com>
2026-05-12 11:59:25 -0400

    added keymap to open quickfix list

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,8 @@
--- lazy keymaps
+-- lazy
 vim.keymap.set("n", "<leader>l", ":Lazy<CR>", { noremap = true, silent = true })
 
--- misc keymaps
+-- misc
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP rename" })
+
+-- quickfix
+vim.keymap.set("n", "<leader>q", ":copen<CR>", { noremap = true, silent = true })
