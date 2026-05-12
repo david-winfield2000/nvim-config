482e66f3562f4ba36f43a3deb53ee08128562c39
David Winfield <david.winfield2000@gmail.com>
2026-05-12 13:07:55 -0400

    reordered keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,8 @@
 -- lazy
 vim.keymap.set("n", "<leader>l", ":Lazy<CR>", { noremap = true, silent = true })
 
--- misc
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP rename" })
-
 -- quickfix
 vim.keymap.set("n", "<leader>q", ":copen<CR>", { noremap = true, silent = true })
+
+-- misc
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP rename" })
