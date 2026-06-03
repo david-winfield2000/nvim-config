ab6a3ce579a099ca21687b9dd3a5aff6eb6c70c4
David Winfield <david.winfield2000@gmail.com>
2026-06-03 00:56:11 -0400

    updated to hide git ignored files and dotfiles unless tracked

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,7 +1,39 @@
+local function parse_output(proc)
+	local result = proc:wait()
+	local ret = {}
+	if result.code == 0 then
+		for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+			line = line:gsub("/$", "")
+			ret[line] = true
+		end
+	end
+	return ret
+end
+
+local function new_git_status()
+	return setmetatable({}, {
+		__index = function(self, key)
+			local ignore_proc = vim.system(
+				{ "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" },
+				{ cwd = key, text = true }
+			)
+			local tracked_proc = vim.system({ "git", "ls-tree", "HEAD", "--name-only" }, { cwd = key, text = true })
+
+			local ret = {
+				ignored = parse_output(ignore_proc),
+				tracked = parse_output(tracked_proc),
+			}
+
+			rawset(self, key, ret)
+			return ret
+		end,
+	})
+end
+
+local git_status = new_git_status()
+
 return {
 	"stevearc/oil.nvim",
-	---@module 'oil'
-	---@type oil.SetupOpts
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	lazy = false,
 	keys = {
@@ -11,19 +43,21 @@ return {
 		watch_for_changes = true,
 		view_options = {
 			is_hidden_file = function(name, bufnr)
-				-- normal files
-				if name == ".." or name == ".git" then
+				if name == ".." then
 					return true
 				end
 
-				-- mac files
-				if name == ".DS_Store" then
-					return true
+				local dir = require("oil").get_current_dir(bufnr)
+				local is_dotfile = vim.startswith(name, ".") and name ~= ".."
+
+				if not dir then
+					return is_dotfile
 				end
 
-				-- python files
-				if name == "venv" or name == "__pycache__" or name:match("%.pyc") then
-					return true
+				if is_dotfile then
+					return not git_status[dir].tracked[name]
+				else
+					return git_status[dir].ignored[name]
 				end
 			end,
 		},
