06889e51e4e946b5219b8f46df0a7905b425ef89
David Winfield <david.winfield2000@gmail.com>
2026-06-06 17:36:23 -0400

    updated keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,6 +6,9 @@ vim.keymap.set("n", "<leader>q", ":copen<CR>", { noremap = true, silent = true }
 
 -- misc
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP rename" })
+vim.keymap.set("n", "gD", vim.lsp.buf.definition, {
+	desc = "Go to Definition",
+})
 
 -- recenter when making large jumps
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
